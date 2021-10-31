7268f785bfca941216c61e495f8e019a28392be3
Zach LeBlanc <zachleblanc@chevron.com>
2021-10-31 17:45:48 -0500

    add docs to bg script

--- background.js
+++ background.js
@@ -1,10 +1,12 @@
+// Listen for content scripts requestion ADO oauth token
 chrome.runtime.onMessage.addListener((message, sender, callback) => {
     chrome.cookies.get({ url: message.domain, name: message.name }).then(callback);
     return true;
 });
 
+// Handle navigation to pipeline runs view via history updates
+// Ex: clicking the back button
 const regex = new RegExp(/https:\/\/dev\.azure\.com\/.*_build\?.*definitionId=.*/,'i');
-
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
     if(regex.test(details.url) && details.frameId == 0 && details.transitionType == 'reload') {
         chrome.tabs.get(details.tabId, function(tab) {
