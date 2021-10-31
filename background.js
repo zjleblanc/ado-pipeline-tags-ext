38c26d09e55038042fc356a37625fe02e4bbaff2
Zach LeBlanc <zachleblanc@chevron.com>
2021-10-31 17:42:14 -0500

    handle back button / page reload and add documentation

--- background.js
+++ background.js
@@ -2,3 +2,22 @@ chrome.runtime.onMessage.addListener((message, sender, callback) => {
     chrome.cookies.get({ url: message.domain, name: message.name }).then(callback);
     return true;
 });
+
+const regex = new RegExp(/https:\/\/dev\.azure\.com\/.*_build\?.*definitionId=.*/,'i');
+
+chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+    if(regex.test(details.url) && details.frameId == 0 && details.transitionType == 'reload') {
+        chrome.tabs.get(details.tabId, function(tab) {
+            if(tab.url === details.url) {
+                chrome.scripting.insertCSS({
+                    target: { tabId: tab.id },
+                    files: ['css/main.css']
+                });    
+                chrome.scripting.executeScript({
+                    target: { tabId: tab.id },
+                    files: ['include/jquery-3.6.0.slim.min.js','scripts/main.js']
+                });    
+            }
+        });
+    }
+});
--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,36 @@
-let oauth_token = "UNSET";
-let rowQuery = "table.runs-table > tbody > a[role=row]";
-let rows = $(rowQuery);
+/**
+ * Wait for element to exist in DOM
+ * @param {string} query - selector for target element(s).
+ * @param {function} callback - callback function which receives the existing element(s).
+ * @param {number} interval - ms between check for element(s) to exist.
+ */
+function waitForElements(query, callback, interval=100){
+    var elementsExist = setInterval(function(){
+        let elements = $(query)
+        if(elements.length) {
+            callback(elements);
+            clearInterval(elementsExist);
+        }
+    }, interval);
+}
 
+/**
+ * Get a list of ADO build ids from <a> elements
+ * @param {list} builds - list of <a> elements which represent builds.
+ * @returns {list} list of build ids
+ */
 function extractBuildIds(builds) {
     return $.map(builds, function(val) {
         let url = val.href;
-        let buildId = url.split("buildId=")[1];
-        val.classList.add(buildId)
         return url.split("buildId=")[1];
     });
 }
 
+/**
+ * Create a new <span> element to represent a list of build tags
+ * @param {list} tags - list of builds tags retrieved from ADO API.
+ * @returns {list} <span> element containing bolt pills to display tags.
+ */
 function createElement(tags){
     let pillsContainer = $('<span class="ext-gen-tags flex-row flex-center">');
     $.each(tags, function(idx, tag){
@@ -21,12 +41,19 @@ function createElement(tags){
     return pillsContainer;
 }
 
-async function addElements(buildIds) {
+/**
+ * Retrieve build details from ADO API, create bolt pills, and inject html into runs table
+ * @async
+ * @param {list} buildIds - list of build ids to query details from ADO API.
+ * @param {list} token - ADO oauth token sourced from cookies
+ */
+async function addElements(buildIds, token) {
+    let baseUrl = window.location.toString().split("_build")[0];
     let query = buildIds.join(",")
     fetch(baseUrl + "_apis/build/builds?api-version=5.1&buildIds=" + query, {
         method: 'get',
         headers: {
-            'Authorization': 'Bearer ' + oauth_token
+            'Authorization': 'Bearer ' + token
         }
     })
     .then(function(response) {
@@ -34,20 +61,47 @@ async function addElements(buildIds) {
     })
     .then(function(json){
         $.each(json.value, function(idx, build) {
-            $("a." + build.id + " > td[data-column-index=1] > div.bolt-table-cell-content > div.flex-row")
-                .append(createElement(build.tags));
+            let buildQuery = "a[href$=" + build.id + "] > td[data-column-index=1] > div.bolt-table-cell-content > div.flex-row";
+            waitForElements(buildQuery, function(builds){
+                builds.append(createElement(build.tags));
+            });
         });
     });
 }
 
-let baseUrl = window.location.toString().split("_build")[0];
-chrome.runtime.sendMessage({domain: "https://dev.azure.com/*", name: "UserAuthentication"}, function(cookie) {
-    oauth_token = cookie.value;
-    addElements(extractBuildIds(rows));
-});
+/**
+ * Bind functionality to add build tags to new builds inserted into the DOM
+ */
+function bindToNewBuilds() {
+    waitForElements('table.runs-table', function(tbl){
+        tbl.bind('DOMNodeInserted', function(e) {
+            if(e.target.parentNode.localName == "tbody"){
+                addElements(extractBuildIds([e.target]))
+            }
+        });
+    });
+}
 
-$("table.runs-table > tbody").bind('DOMNodeInserted', function(e) {
-    if(oauth_token != "UNSET" && e.target.parentNode.localName == "tbody"){
-        addElements(extractBuildIds([e.target]))
+chrome.storage.local.get(['ado_token'], function(result){
+    let rowQuery = "table.runs-table > tbody > a[role=row]";
+
+    // ADO Token exists in local storage
+    if("ado_token" in result)
+    {
+        waitForElements(rowQuery, function(rows){
+            addElements(extractBuildIds(rows), result.ado_token);
+            bindToNewBuilds();
+        });
+    }
+    // ADO token needs to be loaded from cookies
+    else 
+    {
+        chrome.runtime.sendMessage({domain: "https://dev.azure.com/*", name: "UserAuthentication"}, function(cookie) {
+            oauth_token = cookie.value;
+            chrome.storage.local.set({ado_token: cookie.value}, function(){
+                addElements(extractBuildIds($(rowQuery)), cookie.value);
+                bindToNewBuilds();
+            });
+        });
     }
 });
