75ce9ef6cd6fe550e431eb416e3353f1b0213fe0
Zach LeBlanc <zjleblanc3@gmail.com>
2022-11-21 14:22:44 -0600

    fix tooltip on hover and remove dupe values

--- scripts/main.js
+++ scripts/main.js
@@ -15,6 +15,22 @@ function waitForElements(query, callback, interval=100){
 }
 
 /**
+ * Wait for element to exist in DOM
+ * @param {string} query - selector for target element(s).
+ * @param {function} callback - callback function which receives the existing element(s).
+ * @param {number} interval - ms between check for element(s) to exist.
+ */
+ function waitForChildElements(parent, query, callback, interval=100){
+    var elementsExist = setInterval(function(){
+        let elements = parent.find(query);
+        if(elements.length) {
+            callback(elements);
+            clearInterval(elementsExist);
+        }
+    }, interval);
+}
+
+/**
  * Get a list of ADO build ids from <a> elements
  * @param {list} builds - list of <a> elements which represent builds.
  * @returns {list} list of build ids
@@ -29,16 +45,53 @@ function extractBuildIds(builds) {
 /**
  * Create a new <span> element to represent a list of build tags
  * @param {list} tags - list of builds tags retrieved from ADO API.
- * @returns {list} <span> element containing bolt pills to display tags.
+ * @returns {Element} <span> element containing bolt pills to display tags.
  */
-function createElement(tags){
+function createBuildTagsElement(tags){
     let pillsContainer = $('<span class="ext-gen-tags flex-row flex-center">');
-    $.each(tags, function(idx, tag){
+    tags.forEach(function(tag){
         let pillWrapper = $('<div class="margin-right-4 bolt-pill flex-row flex-center standard compact" role="presentation" aria-label="' + tag + '">');
         pillWrapper.append($('<div class="bolt-pill-content text-ellipsis">' + tag + "</div>"));
         pillsContainer.append(pillWrapper);
     });
+    bindTooltip(pillsContainer, tags);
     return pillsContainer;
+}
+
+/**
+ * Create a new <bolt-popover> element to show all build tags
+ * @returns {Element} <bolt-popover> element representing button+popover
+ */
+ function createTooltipContainer(){
+    if($('.tag-list-tooltip').length) {
+        return;
+    }
+    let tooltip = $('<div class="tag-list-tooltip">');
+    let container = $('.region-page');
+    container.append(tooltip);
+    return container;
+}
+
+function populateTooltip(tags) {
+    let tooltip = $('.tag-list-tooltip');
+    let tagList = $('<dl class="tag-dl">');
+    tags.forEach((tag) => {
+        let tagElement = $('<dt class="tooltip-tag">');
+        tagElement.text(tag);
+        tagList.append(tagElement);
+    });
+    tooltip.children().remove();
+    tooltip.append(tagList);
+}
+
+function bindTooltip(element, tags) {
+    let tooltip = $('.tag-list-tooltip');
+    element.mouseenter(() => {
+        populateTooltip(tags);
+        tooltip.css('visibility', 'visible');
+    }).mouseleave(() => {
+        tooltip.css('visibility', 'hidden');
+    });
 }
 
 /**
@@ -61,9 +114,26 @@ async function addElements(buildIds, token) {
     })
     .then(function(json){
         $.each(json.value, function(idx, build) {
-            let buildQuery = "a[href$=" + build.id + "] > td[data-column-index=1] > div.bolt-table-cell-content > div.flex-row";
-            waitForElements(buildQuery, function(builds){
-                builds.append(createElement(build.tags));
+            let buildRowQuery = `a[href$="buildId=${build.id}"]`;
+            let buildTagColQuery = "td[data-column-index=1] > div.bolt-table-cell-content > div.flex-row"
+            waitForElements(buildRowQuery, function(builds){
+                builds.first().addClass('table-cell-ext-gen-tags-mod');
+                waitForChildElements(builds.first(), buildTagColQuery, function(cols) {
+                    let buildTagsCol = cols.first();
+
+                    let existingTooltip = buildTagsCol.children('.ext-gen-tags-tooltip');
+                    if(existingTooltip.length) {
+                        existingTooltip.remove();
+                    }
+                    let uniqueTags = new Set(build.tags);
+                    // buildTagsCol.append(createTagListTooltipElement(uniqueTags));
+
+                    let existingTags = buildTagsCol.children('.ext-gen-tags');
+                    if(existingTags.length) {
+                        existingTags.remove();
+                    }
+                    buildTagsCol.append(createBuildTagsElement(uniqueTags));
+                })
             });
         });
     });
@@ -92,6 +162,8 @@ chrome.storage.local.get(['ado_token'], function(result){
             addElements(extractBuildIds(rows), result.ado_token);
             bindToNewBuilds();
         });
+
+        createTooltipContainer();
     }
     // ADO token needs to be loaded from cookies
     else 
